2fbfa3881507630b8abd1edce99e0b03026f7bf4
Kato4B61746F <yurugi.kato@nifty.com>
2022-09-23 12:27:27 +0900

    投稿にコメントを投稿し表示させる機能を追加しました。

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -4,10 +4,6 @@
 
 <h1>詳細ページ</h1>
 
-
-
-
-    
   @foreach($posts as $post)
     @if ($post->image_path)
       <!-- 画像を表示 -->
@@ -16,9 +12,22 @@
     @endif
   @endforeach
 
+  <form action="/post/store_comment" method="POST">
+      @csrf
 
+      <h2>コメント</h2>
+      <textarea name="comments[comment]" placeholder="かわいいですね"></textarea>
+ 
+      <input type="hidden" name="comments[post_id]" value="{{$post->id}}">
+      <input type="hidden" name="comments[user_id]" value="{{$user->id}}">
+    
+ 
+      <input type="submit" value="保存"/>
+  </form>
+  
+  @foreach($comments as $comment)
+   <p> {{ $comment->comment }}　　user_id= {{ $comment->user_id }}</p>
 
-        
-
+  @endforeach  
 
 @endsection
